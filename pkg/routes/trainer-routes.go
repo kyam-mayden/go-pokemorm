67594730877f658d5a97c2c2b264ac4490d76a1e
Kyam Harris <kyam.lh@googlemail.com>
2021-08-09 08:19:53 +0100

    Add api key auth to all routes

--- pkg/routes/trainer-routes.go
+++ pkg/routes/trainer-routes.go
@@ -8,8 +8,8 @@ import (
 
 var RegisterTrainerStoreRoutes = func(router *mux.Router) {
 	router.Handle("/trainer", middleware.OAuthMiddleware(controllers.GetTrainer)).Methods("GET")
-	router.Handle("/trainer", middleware.BasicAuthMiddleware(controllers.CreateTrainer)).Methods("POST")
-	router.Handle("/trainer/{trainerId}", middleware.BasicAuthMiddleware(controllers.GetTrainerById)).Methods("GET")
+	router.Handle("/trainer", middleware.OAuthMiddleware(controllers.CreateTrainer)).Methods("POST")
+	router.Handle("/trainer/{trainerId}", middleware.OAuthMiddleware(controllers.GetTrainerById)).Methods("GET")
 
 	//router.HandleFunc("/trainer/{trainerId}", controllers.UpdateTrainer).Methods("PUT")
 	//router.HandleFunc("/trainer/{trainerId}", controllers.DeleteTrainer).Methods("DELETE")
