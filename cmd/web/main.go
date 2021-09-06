cc4b4b059a8c790811d9bbf146ca942043fea57a
Kyam Harris <kyam.lh@googlemail.com>
2021-09-06 17:19:23 +0100

    Pull out hardcoded string to config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
 	"github.com/marty-crane/go-pokemorm/cmd/web/routes"
+	"github.com/marty-crane/go-pokemorm/pkg/config"
 )
 
 func main() {
@@ -15,6 +17,9 @@ func main() {
 	routes.RegisterAssetRoutes(r)
 	http.Handle("/", r)
 
-	fmt.Println("Serving on port 8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+    port := config.Get("WEB_PORT")
+
+	fmt.Printf("Serving on port %s", port)
+	fmt.Println()
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost%s", port), r))
 }
